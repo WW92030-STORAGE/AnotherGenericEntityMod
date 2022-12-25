5ffd7b82941bc8dc86710547bb78f71172963cd3
WW92030 <65929722+WW92030-STORAGE@users.noreply.github.com>
2022-12-25 15:39:59 -0600

    Update DarknessXEntity.java

--- src/main/java/com/notasmr/darkness/entity/DarknessXEntity.java
+++ src/main/java/com/notasmr/darkness/entity/DarknessXEntity.java
@@ -1,12 +1,12 @@
 package com.notasmr.darkness.entity;
 
 import com.notasmr.darkness.util.Reference;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityCreature;
-import net.minecraft.entity.EntityLiving;
-import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.*;
 import net.minecraft.entity.ai.*;
 import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.monster.IMob;
+import net.minecraft.entity.passive.EntityPig;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
@@ -14,11 +14,13 @@ import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.MathHelper;
+import net.minecraft.world.EnumDifficulty;
 import net.minecraft.world.World;
 
 import java.util.List;
 
-public class DarknessXEntity extends EntityMob {
+public class DarknessXEntity extends EntityCreature {
 
     public DarknessXEntity(World world) {
         super(world);
@@ -50,6 +52,7 @@ public class DarknessXEntity extends EntityMob {
         this.getEntityAttribute(SharedMonsterAttributes.maxHealth).setBaseValue(40.0F);
         this.getEntityAttribute(SharedMonsterAttributes.movementSpeed).setBaseValue(0.375D);
         this.getEntityAttribute(SharedMonsterAttributes.followRange).setBaseValue(128);
+        this.getAttributeMap().registerAttribute(SharedMonsterAttributes.attackDamage);
         this.getEntityAttribute(SharedMonsterAttributes.attackDamage).setBaseValue(10);
     }
 
@@ -72,6 +75,12 @@ public class DarknessXEntity extends EntityMob {
 
     public void onEntityUpdate() {
         super.onEntityUpdate();
+
+        if (!this.worldObj.isRemote && this.worldObj.difficultySetting == EnumDifficulty.PEACEFUL)
+        {
+            this.setDead();
+        }
+
         if (DEBUG0) return;
         World level = this.worldObj;
         AxisAlignedBB aabb = AxisAlignedBB.getBoundingBox(this.posX - radius, 0, this.posZ - radius, this.posX + radius, 256, this.posZ + radius);
@@ -175,5 +184,47 @@ public class DarknessXEntity extends EntityMob {
         x(s, player);
 
         return true;
+    }
+
+
+    // Copied directly from EntityMob
+    public boolean attackEntityAsMob(Entity p_70652_1_)
+    {
+        float f = (float)this.getEntityAttribute(SharedMonsterAttributes.attackDamage).getAttributeValue();
+        int i = 0;
+
+        if (p_70652_1_ instanceof EntityLivingBase)
+        {
+            f += EnchantmentHelper.getEnchantmentModifierLiving(this, (EntityLivingBase)p_70652_1_);
+            i += EnchantmentHelper.getKnockbackModifier(this, (EntityLivingBase)p_70652_1_);
+        }
+
+        boolean flag = p_70652_1_.attackEntityFrom(DamageSource.causeMobDamage(this), f);
+
+        if (flag)
+        {
+            if (i > 0)
+            {
+                p_70652_1_.addVelocity((double)(-MathHelper.sin(this.rotationYaw * (float)Math.PI / 180.0F) * (float)i * 0.5F), 0.1D, (double)(MathHelper.cos(this.rotationYaw * (float)Math.PI / 180.0F) * (float)i * 0.5F));
+                this.motionX *= 0.6D;
+                this.motionZ *= 0.6D;
+            }
+
+            int j = EnchantmentHelper.getFireAspectModifier(this);
+
+            if (j > 0)
+            {
+                p_70652_1_.setFire(j * 4);
+            }
+
+            if (p_70652_1_ instanceof EntityLivingBase)
+            {
+                EnchantmentHelper.func_151384_a((EntityLivingBase)p_70652_1_, this);
+            }
+
+            EnchantmentHelper.func_151385_b(this, p_70652_1_);
+        }
+
+        return flag;
     }
 }
